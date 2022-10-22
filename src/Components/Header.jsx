61fb34ce3d9d3941a0ef43672422919768ecd188
sanduni <cst18045@std.uwu.ac.lk>
2022-10-22 17:48:49 +0530

    first file

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
+import Logo from './Images/logo.png'
 
 const Header = () => {
   return (
-      <div className="fixed z-50 w-screen bg-slate-300 p-6 px-16">
+    <header className='fixed z-50 w-screen p-6 px-16'>
+     
           
-          <div className ="hidden md:flex w-full h-full bg-green-500 p-4"></div>
-
+       <div className ="hidden md:flex w-full h-full  p-4">
+      <div className='flex items-center gap-2'>
         
-          <div className="flex md:hidden w-full h-full bg-blue-500 p-4"> </div>
+      <img src={Logo} className= "w-8 object-cover" alt="logo"/>
+        </div>
+        <p className="text-heading to-blue-600 text-x1 font-bold"> City </p>
+         
           
-
+      <ul className="">
+        <li>Home</li>
+        <li>Menu</li>
+        <li>About Us</li>
+        <li>Services</li>
+          </ul>
          </div>
-                  
-                                                                                                                                                                                 
+       <div className="flex md:hidden w-full h-full p-4"> </div>          
+   </header>                                                                                                                                                                             
   );
 };
-
 export default Header;
